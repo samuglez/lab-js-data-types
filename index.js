58e0a2aa3e80e1d71a0e929aa1dde6d5e71a0fe0
unknown <samuglez90@gmail.com>
2025-01-15 15:41:42 +0100

    Resuelto Samuel

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+let tongueTwister =`${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+let result
+result = part1.slice(0,3)+part1[3].toUpperCase()+
+ part2.slice(0,5)+ part2[5].toUpperCase()
 
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+let tipAmount = `${billTotal}`* 15 / 100
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,11 +49,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+let randomNumber = Math.floor(Math.random()* (10-1)+1)
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +63,24 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; //False
 
-const expression2 = a || b;
+const expression2 = a || b;//True
 
-const expression3 = !a && b;
+const expression3 = !a && b;//False
 
-const expression4 = !(a && b);
+const expression4 = !(a && b);//True
 
-const expression5 = !a || !b;
+const expression5 = !a || !b;//True
 
-const expression6 = !(a || b);
+const expression6 = !(a || b);//True
 
-const expression7 = a && a;
+const expression7 = a && a;//True
+
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
